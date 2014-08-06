77042833127ac511e7c78f8a2f7d7a14f09ec2d2
Abhishek Pratap <abhishek.vit@gmail.com>
2014-08-06 09:58:20 -0700

    latest version

--- R_Scripts/1a_illuminaExp_publicData_org.R
+++ R_Scripts/1a_illuminaExp_publicData_org.R
@@ -1,87 +1,52 @@
-source("http://bioconductor.org/biocLite.R")
-biocLite (c("beadarray", "limma", "GEOquery", "illuminaHumanv1.db",
-             "illuminaHumanv2.db", "illuminaHumanv3.db", "BeadArrayUseCases" ))
-biocLite("illuminaHumanv4.db")
-biocLite (c( "GOstats", "GenomicRanges", "Biostrings" ))
+code_base_dir <- paste0(path.expand("~"),"/dev/Synodos_NF2/R_Scripts/" )
 
+#source("http://bioconductor.org/biocLite.R")
+#biocLite (c("beadarray", "limma", "GEOquery", "illuminaHumanv1.db",
+#             "illuminaHumanv2.db", "illuminaHumanv3.db", "BeadArrayUseCases" ))
+#biocLite("illuminaHumanv4.db")
+#biocLite (c( "GOstats", "GenomicRanges", "Biostrings" ))
 
 
 library("GEOquery")
 library("tools")
-library("frma")
-library("affy")
 library("synapseClient")
+library("beadarray")
+library("illuminaHumanv4.db")
 synapseLogin()
 
-#
-expressionData_synfolder_id <- "syn2481212"
+#call the GEO functions
+source(paste0(code_base_dir,"/lib/GEO_functions.R"))
+
+
+#synid of the folder where the expression data will be stored
+meningioma_studies_bucket <- "syn2537450"
+schwannoma_studies_bucket <- "syn2537451"
 
 #this script github URL
-thisscript_URL <- "https://github.com/Sage-Bionetworks/Synodos_NF2/blob/master/R_Scripts/1_publicData_org.R"
-
-extractGEOdata <- function(f){
-  if(file_ext(f) == "tar"){
-    untar(f, exdir=dirname(f))
-  }
-}
+thisscript_URL <- "https://github.com/Sage-Bionetworks/Synodos_NF2/blob/master/R_Scripts/1a_illuminaExp_publicData_org.R"
 
 
-
-get_GEO_ftpLink <- function(GEO){
-  geotype <- toupper(substr(GEO, 1, 3))
-  stub = gsub("\\d{1,3}$", "nnn", GEO, perl = TRUE)
-  if (geotype == "GSM") {
-    url <- sprintf("ftp://ftp.ncbi.nlm.nih.gov/geo/samples/%s/%s/suppl/", 
-                   stub, GEO)
-  } else if (geotype == "GSE") {
-    url <- sprintf("ftp://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/suppl/", 
-                   stub, GEO)
-  } else if (geotype == "GPL") {
-    url <- sprintf("ftp://ftp.ncbi.nlm.nih.gov/geo/platform/%s/%s/suppl/", 
-                   stub, GEO)
-  } else {
-    url <- None
-  }
-}
-
-get_formatted_phenoData <- function(geo_study){
-  df <- pData(geo_study)
-  idx <- grep("characteristics", colnames(df), fixed=T, value=T)
-  df <- df[,idx]
-  for(i in idx){
-    tmp <- strsplit(as.character(df[[i]]), ": ", fixed=T)
-    nm <- sapply(tmp, "[[", 1)
-    df[[unique(nm)]] <- sapply(tmp, "[[", 2)
-    df[[i]] <- NULL
-  }
-  df['sampleId'] <- rownames(df)
-  return(df)
-}
-
-
-######################
+#####################
 ##MAIN
 ######################
 
 #specify the ID of the GEO study to download and analyze
-geo_id = 'GSE58037'
+geo_id = 'GSE56157'
 
 #create a folder 
-study_folder <- synStore(Folder(name=geo_id, parentId = expressionData_synfolder_id ))
+study_folder <- synStore(Folder(name=geo_id, parentId = meningioma_studies_bucket ))
 #push the study page as a reference
 source_url = get_GEO_ftpLink(geo_id)
 source_url = synStore(File(source_url, name=geo_id, synapseStore=F, parentId=study_folder$properties$id))
 
-orig_working_dir <- getwd()
 base_dir <- "/external-data/DAT_123__Synodos_NF2/publicData//expressionData/"
 setwd(base_dir)
-
 #download the raw data
-df <- getGEOSuppFiles(geo_id)
-df
-setwd(orig_working_dir)
-rownames(df)
-sapply(rownames(df),extractGEOdata) 
+#df <- getGEOSuppFiles(geo_id)
+# setwd(orig_working_dir)
+# getwd()
+# rownames(df)
+# sapply(rownames(df),extractGEOdata) 
 
 
 #get the phenotype data
@@ -92,19 +57,51 @@ phenotype_data_file <- paste0(base_dir, geo_id, '/', geo_id, '_phenotype_data.ts
 write.table(phenotype_data, phenotype_data_file, col.names=T, row.names=F, quote=FALSE, sep="\t")
 phenotype_data_file <- synStore(File(phenotype_data_file, parentId =study_folder$properties$id ), used= source_url$properties$id)
 
-exprs(geo_study)
 
-#process the expression data
-affyBatch_object <- ReadAffy(celfile.path=dirname(rownames(df)[1]))
-#num probes
-#dim(pm(affyBatch_object))
-normData <- frma(affyBatch_object, summarize="random_effect")
+geo_study <- addFeatureData(geo_study, 
+                            toAdd = c("SYMBOL", "PROBEQUALITY", "CODINGZONE", "PROBESEQUENCE", "GENOMICLOCATION"),
+                            annotation="Humanv4")
 
-geneExp_raw <- exprs(affyBatch_object)
-geneExp <- exprs(normData)
+#get the feature names of the illumina array
+ids <- as.character(featureNames(geo_study))
+probe_quality <- unlist(mget(ids, illuminaHumanv4PROBEQUALITY, ifnotfound=NA))
+
+#remove low quality probes
+to_remove <- probe_quality == "No match" | probe_quality == "Bad" | is.na(probe_quality)
+#remove the bad probes from the downstream analysis
+geo_study.flt <- geo_study[!to_remove,]
+
+#create  a matrix of gene expression
+geneExp <- exprs(geo_study.flt)
+
+#convert to illumina probe id's to geneNames(symbols)
+geneNames <- unlist(mget(rownames(geneExp),illuminaHumanv4SYMBOL, ifnotfound=NA))
+rownames(geneExp) <- geneNames
+
+#remove the rows where the genename is NA
+rows_to_keep <- !is.na(rownames(geneExp))
+geneExp <- geneExp[rows_to_keep,]
+
+#take avg of the rows for the same geneNames
+#multiple probes mapping to a same gene are summarized (mean)
+avg_genes <- function(gene){
+  e <- geneExp[rownames(geneExp) %in% gene,]
+  apply(e,2,mean)
+}
+
+duplicate_genes <- names(which(table(rownames(geneExp)) > 1))
+avg_geneExp_duplicate_genes <- sapply(duplicate_genes,avg_genes)
+avg_geneExp_duplicate_genes <- t(avg_geneExp_duplicate_genes)
+
+#keep only the genes which have one unique probe
+geneExp <- geneExp[(!rownames(geneExp) %in% duplicate_genes),]
+
+#now add the avg expression values for genes with multiprobes
+geneExp <- rbind(geneExp, avg_geneExp_duplicate_genes)
+
 
 exp_file <- paste0(base_dir, geo_id, '/', geo_id, '_expvals.txt')
-write.table(geneExp, file=exp_file, col.names=T, row.names=T, quote=F)
+write.table(geneExp, file=exp_file, col.names=T, row.names=T, quote=F, sep="\t")
 exp_file <- synStore(File(exp_file, parentId=study_folder$properties$id),
                      used=source_url$properties$id,
                      executed=thisscript_URL)
@@ -112,13 +109,16 @@ exp_file <- synStore(File(exp_file, parentId=study_folder$properties$id),
 #boxplot
 boxplot_file <- paste0(base_dir, geo_id, '/', geo_id, '_expBoxplot.png')
 png(boxplot_file, width=8, height=6, units="in", res=200)
-boxplot(geneExp, xlab="CEL File", ylab="expr", main=paste0('GEO Study ', geo_id, ' expression boxplot' ))
+boxplot(geneExp, xlab="sample", ylab="expr", main=paste0('GEO Study ', geo_id, ' expression boxplot' ))
 dev.off()
 boxplot_file <- synStore(File(boxplot_file, parentId=study_folder$properties$id), 
                          used=source_url$properties$id,
                          executed=thisscript_URL)
 
 
+#####
+#extra code
+#####
 boxplot(log2(geneExp_raw), xlab="CEL File", ylab="expr", 
         main=paste0('GEO Study ', geo_id, ' expression boxplot' ))
 
@@ -129,7 +129,6 @@ phenotype_data
 boxplot(geneExp, xlab="CEL File", ylab="expr", 
         col = factor(phenotype_data$tissue),
         main=paste0('GEO Study ', geo_id, ' expression boxplot' ))
-
 
 
 
