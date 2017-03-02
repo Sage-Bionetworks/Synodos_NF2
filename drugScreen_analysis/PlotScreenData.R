59b208b9fbc490b7c634b419249b87e2c80a790d
allaway <robert.allaway@sagebase.org>
2017-03-01 16:46:06 -0800

    updated analysis

--- drugScreen_analysis/PlotScreenData.R
+++ drugScreen_analysis/PlotScreenData.R
@@ -28,7 +28,10 @@ ggplot(sub2, aes(x= HS01, y=HS12)) +
     geom_point(stat = "identity", aes(color=(HS01/HS12))) + 
     coord_fixed() +
     scale_color_viridis() +
-    geom_text(data=subset(sub2, HS01/HS12>4), aes(label = Gene.Name), size = 2)
+    geom_text(data=subset(sub2, HS01/HS12>4), aes(label = Compound.Name), size = 2)
+
+intersect(filter(sub2, HS01/HS12>1.1)$Compound.Name, filter(sub4, HS12/HS01>1.1)$Compound.Name)
+
 
 ggplot(sub2, aes(x= MS02, y=HS12)) +
   geom_point(stat = "identity", aes(color=(MS02/HS12))) + 
@@ -46,7 +49,7 @@ ggplot(sub2, aes(x= Syn6, y=Syn1)) +
   geom_point(stat = "identity", aes(color=(Syn6/Syn1))) + 
   coord_fixed() +
   scale_color_viridis() +
-  geom_text(data=subset(sub2, Syn6/Syn1>4), aes(label = Gene.Name), size = 2)
+  geom_text(data=subset(sub2, Syn6/Syn1>4), aes(label = Compound.Name), size = 2)
 
 
 sub3 <- select(drugdat, Cell.Line, AUC, Compound.Name)
