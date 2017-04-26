047d91636cca4985a9981edc8443e5bdd210cad8
allaway <robert.allaway@sagebase.org>
2017-04-26 16:58:11 -0700

    tcga comparison update

--- TCGA_comparison/getTCGAdata.R
+++ TCGA_comparison/getTCGAdata.R
@@ -1,5 +1,11 @@
 source("cBioPortalData.R")
 library(synapseClient)
+library(data.table)
+synapseLogin()
+
+ex2 <- "https://raw.githubusercontent.com/Sage-Bionetworks/Synodos_NF2/master/TCGA_comparison/getTCGAdata.R"
+
+##TCGA function source from Sara Gosline
 exp <- getDisExpressionData(study = "tcga")
-write.table("expression.txt", sep = "t")
-synStore(File("expression.txt", parentID= "syn9705598"), executed = c(this.file, 
+fwrite(exp, "TCGAexpressionMatrix.txt", sep = "\t")
+synStore(File("TCGAexpressionMatrix.txt", parentId= "syn9705598"), executed = c(ex2)) 
